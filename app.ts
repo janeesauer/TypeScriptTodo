a28f13e62e243f2c0633025ec823da6c46ecdc2e
Jane Sauer <jane.sauer@coveainsurance.co.uk>
2021-04-14 13:44:59 +0100

    ch 3.4

--- app.ts
+++ app.ts
@@ -1,6 +1,17 @@
-// the array can be any as we are only interested in the length of the array.
-// typing total as number ensures that id the returned type is changed for any reason there is another type check in place.
-function totallength(x: any[], y: string): number {
-  var total: number = x.length + y.length;  // method
+// union types using pipe operator |
+// wrap union types in paretheses to mak it cleaner
+//
+function totallength(x: (string | any[]), y: (string | any[]) ): number { // union types
+  var total: number = x.length + y.length;  
+
+  x.slice(0);
+
+  if ( x instanceof Array ) {   // typeguard syntax
+    x.push('abc');
+  }
+
+  if ( typeof x === 'string' ) {  // syntax reserved for primative types  or   if( x instanceof String ) - String class
+    x.substr(1);
+  }
   return total;
-}
+};
