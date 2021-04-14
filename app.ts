6ffa2f9987eab26edbac13dab6a9bdc95eab64b6
Jane Sauer <jane.sauer@coveainsurance.co.uk>
2021-04-14 13:57:57 +0100

    ch 3.5

--- app.ts
+++ app.ts
@@ -1,7 +1,10 @@
-// union types using pipe operator |
-// wrap union types in paretheses to mak it cleaner
-//
-function totallength(x: (string | any[]), y: (string | any[]) ): number { // union types
+// signatures
+// 
+//overloaded function with alternate signatures that we want to expose
+
+function totalLength(x: string, y:string): number 
+function totalLength(x: any[], y:any[]): number
+function totalLength(x: (string | any[]), y: (string | any[]) ): number { // union types
   var total: number = x.length + y.length;  
 
   x.slice(0);
@@ -15,3 +18,8 @@ function totallength(x: (string | any[]), y: (string | any[]) ): number { // uni
   }
   return total;
 };
+
+// when i consume this function it shows the two overload signatures to pass 2 strings or 2 arrays
+totalLength()
+// overloads are removed when converted to javascript 
+// function overloads in typescript can be viewed as metadata to define
